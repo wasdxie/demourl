67c842c02e2e8d19e6b0521d9370720b9b3d4235
wasdxie <xiexienideak47@163.com>
2019-09-01 14:26:27 +0800

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,4 +1,4 @@
-const validate = (params,rule) => {
+const validate = (params,rule) => {//params:要验证的参数,rule:验证的规则
     return function (target, name, descriptor) {
         const oldValue = descriptor.value；//保存老的
         descriptor.value = function(){
