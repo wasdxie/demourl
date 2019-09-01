82ae6f24227b6459d06a6b0e2c5651a505825ca1
wasdxie <xiexienideak47@163.com>
2019-09-01 14:18:27 +0800

    Update and rename validateRule.js to validate.js

---
+++ validate.js
@@ -0,0 +1,16 @@
+const validate = params => rule => {
+    return function (target, name, descriptor) {
+        const oldValue = descriptor.value；//保存老的
+        descriptor.value = function(){
+           if(验证不通过){
+                //做一些事情
+           }else{
+            oldValue.apply(target,arguments)
+           }
+            
+        }
+        return descriptor;
+    }
+}
+
+
