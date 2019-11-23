f3f2910c8eb4dc1c5f78a0bbd3734d1ac4f9f397
wasdxie <xiexienideak47@163.com>
2019-11-23 22:44:12 +0800

    Update sort.js

--- sort.js
+++ sort.js
@@ -18,3 +18,24 @@ function maopai(arr){
 }
 
 maopai([9,8,7,6,5,4,3,2,1])
+
+
+function maopai2(arr){
+    let len = arr.length;
+    for(let i=len;i>=2;i--){//排序次数
+        let flag = true;
+        for(let j=0;j<i-1;j++){//每次排序,每次排序高位都是有序的
+            if(arr[j]>arr[j+1]){
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+                flag = false;
+            }
+        }
+        if(flag){
+            console.log(111);
+            break;
+        }
+    }
+    console.log(arr);
+}
+
+maopai2([9,8,7,6,5,4,3,2,1])
